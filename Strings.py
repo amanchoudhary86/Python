b2625483abadc5ff7027b0304558da6c8f0ecb2e
amanchoudhary86 <amanchoudhary.1502@gmail.com>
2024-07-17 23:35:14 +0530

    Fourth Commit

---
+++ Strings.py
@@ -0,0 +1,18 @@
+st = "---Aman is a good boi---"
+print(len(st))
+
+print(st[::])
+
+print(st.lower())
+
+print(st.upper())
+
+print(st.count("a"))
+
+print(st.find("b"))
+
+print(st.strip("-"))
+
+print(st.lstrip("-"))
+
+print(st.rstrip("-"))
