84bd320d8d9a6cdde9ef053df073222b977ab14d
amanchoudhary86 <amanchoudhary.1502@gmail.com>
2024-07-18 09:48:18 +0530

    Latest commit

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,26 @@
+#This is a comment and it is the non-executionable part of the code which is used to mention whatever the programmer wants to. Ex: Reference, notes, warnings, etc.
+
+
 # To diplay output on the console:
 print("Hello World")
+
+# To take an input on the console:
+input("Exter your value here : ")
+
+# Variables: Variables are containers which contain some value. This value can be on many types and will be discussed further.
+
+# Rules to define a variable:
+# 1. Variable name should start with a letter or an underscore.
+# 2. Variable name can contain only letters, numbers, and underscores.
+# 3. Variable name should not contain any special characters.
+# 4. Variable name should not contain any space.
+# 5. Variable name should not be a keyword.
+
+# Each variable will be studied in the further modules(files) with examples and direct executable steps
+
+# Creating a variable which takes input and then displays it to the console:
+VariableName = input("Input your number here : ")
+
+print("Your number is : ", VariableName) # A comma is used to print together two or more strings,integers, etc.
+
+# This module Ends Here
