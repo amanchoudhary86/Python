11a2ef7fcf375c25c8256ce20c41757807ccea6f
amanchoudhary86 <amanchoudhary.1502@gmail.com>
2024-07-19 16:09:20 +0530

    Latest commit

---
+++ 7_Type_Casting.py
@@ -0,0 +1,14 @@
+# Type Casting:-
+
+# Type casting is the process of converting one data type into another data type. Type casting is also known
+# as type conversion.
+
+# Before Type Casting:
+a = 20
+
+print("Before Type Casting: ",type(a))
+
+# After Type Casting:
+a = str(a)
+
+print("After Type Casting: ",type(a))
