eb89b740c2c9587a6a9b5d9e50e794faf470c794
amanchoudhary86 <amanchoudhary.1502@gmail.com>
2024-07-20 19:34:29 +0530

    Latest Commit

--- 9_Operators.py
+++ 9_Operators.py
@@ -13,3 +13,38 @@
 # Special operators-->  lambda, @, :=, **=, //=
 
 # Python operators precedence(The order in which we give them priority order)
+# 1. Parentheses: (),[],{}
+# 2. Exponentiation: **
+# 3. Multiplication(*), Division(/), Modulus(%)
+# 4. Addition, Subtraction
+# 5. Comparison
+# 6. Logical AND
+# 7. Logical OR
+# 8. Assignment
+# 9. Identity
+# 10. Membership
+# 11. Bitwise AND
+# 12. Bitwise XOR
+# 13. Bitwise OR
+# 14. Bitwise Left Shift
+# 15. Bitwise Right Shift
+# 16. Unary Plus
+# 17. Unary Minus
+# 18. Unary Bitwise NOT
+# 19. Logical NOT
+# 20. Ternary
+# 21. Special
+# 22. Lambda
+# 23. @
+# 24. :=
+# 25. **=
+# 26. //=
+# 27. %=
+# 28. /=
+# 29. *=
+# 30. +=
+# 31. -=
+# 32. &=
+# 33. ^=
+# 34. |=
+# 35. >>=
