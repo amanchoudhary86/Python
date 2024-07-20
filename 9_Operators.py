7984def9c6d45811149161faf4ff158f3be2192d
amanchoudhary86 <amanchoudhary.1502@gmail.com>
2024-07-20 19:55:04 +0530

    Latest Commit

--- 9_Operators.py
+++ 9_Operators.py
@@ -12,39 +12,44 @@
 # Ternary operators-->  value_if_true if condition1 else condition2
 # Special operators-->  lambda, @, :=, **=, //=
 
-# Python operators precedence(The order in which we give them priority order)
-# 1. Parentheses: (),[],{}
-# 2. Exponentiation: **
-# 3. Multiplication(*), Division(/), Modulus(%)
-# 4. Addition, Subtraction
-# 5. Comparison
-# 6. Logical AND
-# 7. Logical OR
-# 8. Assignment
-# 9. Identity
-# 10. Membership
-# 11. Bitwise AND
-# 12. Bitwise XOR
-# 13. Bitwise OR
-# 14. Bitwise Left Shift
-# 15. Bitwise Right Shift
-# 16. Unary Plus
-# 17. Unary Minus
-# 18. Unary Bitwise NOT
-# 19. Logical NOT
-# 20. Ternary
-# 21. Special
-# 22. Lambda
-# 23. @
-# 24. :=
-# 25. **=
-# 26. //=
-# 27. %=
-# 28. /=
-# 29. *=
-# 30. +=
-# 31. -=
-# 32. &=
-# 33. ^=
-# 34. |=
-# 35. >>=
+# Python operators precedence(The order in which we give them priority order):-
+
+# Priority Order(meaning which are executed first in a single line of code), here is that the priority decreases from top to bottom:-
+
+# 1. Parentheses: `()`, `[]`, `{}`
+# 2. Exponentiation: `**`
+# 3. Multiplication(`*`), Division(`/`), Modulus(`%`)
+# 4. Addition(`+`), Subtraction(`-`)
+# 5. Comparison: `==`, `!=`, `>`, `<`, `>=`, `<=`
+# 6. Logical AND: `and`
+# 7. Logical OR: `or`
+# 8. Assignment: `=`, `+=`, `-=`, `*=`, `/=`, `//=`, `%=`, `**=`
+# 9. Identity: `is`, `is not`
+# 10. Membership: `in`, `not in`
+# 11. Bitwise AND: `&`
+# 12. Bitwise XOR: `^`
+# 13. Bitwise OR: `|`
+# 14. Bitwise Left Shift: `<<`
+# 15. Bitwise Right Shift: `>>`
+# 16. Unary Plus: `+`
+# 17. Unary Minus: `-`
+# 18. Unary Bitwise NOT: `~`
+# 19. Logical NOT: `not`
+# 20. Ternary: `x if condition else y`
+# 21. Special: `del`, `pass`, `continue`, `break`, `return`, `yield`, `raise`, `import`, `from`
+# 22. Lambda: `lambda`
+# 23. `@` (decorator)
+# 24. Assignment Expression: `:=`
+# 25. Exponentiation Assignment: `**=`
+# 26. Floor Division Assignment: `//=`
+# 27. Modulus Assignment: `%=`
+# 28. Division Assignment: `/=`
+# 29. Multiplication Assignment: `*=`
+# 30. Addition Assignment: `+=`
+# 31. Subtraction Assignment: `-=`
+# 32. Bitwise AND Assignment: `&=`
+# 33. Bitwise XOR Assignment: `^=`
+# 34. Bitwise OR Assignment: `|=`
+# 35. Bitwise Right Shift Assignment: `>>=`
+
+# Note : Some of the operators which are on the same priority and in that case the left-most one will be executed first and so on from left to right. 
