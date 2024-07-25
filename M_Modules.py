9d82afc8864970786ead464ba6f124729e74aebd
amanchoudhary86 <amanchoudhary.1502@gmail.com>
2024-07-25 21:09:10 +0530

    25_July,2024

--- M_Modules.py
+++ M_Modules.py
@@ -8,6 +8,10 @@
 import L_functions
 
 # Now we can use the functions ar any other part of code of that module in our file.
+
+# syntax:
+# imported_filename.function_name()
+
 L_functions.addition(1,5,10)
 
 # on executing this code we get the following output:
@@ -18,4 +22,11 @@ L_functions.addition(1,5,10)
 # The sum of  10   11   12  is = 33
 # The sum of  13   14   15  is = 42
 # 10
-# The sum of  1   5   10  is = 16
+# The sum of  1   5   10  is = 16 <-- this is the usage of the addition() function we created in another file in our current file.
+
+# Note: As we imported the file, the whole code was imported here, and that is why we are able to see the output of that file as well, to avoid this and only use the addition() function we use the following method:
+
+from L_functions import addition
+
+print("As you can see below the whole file was not imported, but only the addition() function:-")
+addition(1 , 1 , 1)
