2e91e7f4d1907c6ba5884b3c8f6154a5d0cc2273
amanchoudhary86 <amanchoudhary.1502@gmail.com>
2024-07-26 22:37:09 +0530

    26_July_2024

--- O_Modules_in_python/os.py
+++ O_Modules_in_python/os.py
@@ -2,6 +2,6 @@ import os
 
 # (os stands for operating system)
 
-print(dir(os)) #<-- This prints all the directories present in this module
+print(dir(os) , "<-- This are all the directories present in this module")
 
-print(os.getcwd()) #<-- This prints the current working directory
+print(os.getcwd(), "<-- This is the current working directory")
