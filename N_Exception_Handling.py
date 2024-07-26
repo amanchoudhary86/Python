16d9d724231fe1e54da9b7c9a7aa3ee7001411b3
amanchoudhary86 <amanchoudhary.1502@gmail.com>
2024-07-26 08:58:39 +0530

    26_July_2024

---
+++ N_Exception_Handling.py
@@ -0,0 +1,14 @@
+# Exception Handling :-
+# Exception handling is a mechanism in programming languages that allows developers to manage and respond to runtime errors or unusual conditions that may occur during the execution of a program. Instead of causing the program to crash, exceptions can be caught and handled gracefully, enabling the program to continue running or to fail in a controlled manner.
+
+# What is Exception Handling :- 
+# In programming, things don't always go as planned. For example, you might try to divide a number by zero, or read from a file that doesn't exist. These situations cause errors called "exceptions." Exception handling is a way to catch these errors and do something about them, so your program doesn't crash unexpectedly.
+
+# Why Use Exception Handling :-
+# Prevent Crashes: If your program encounters an error, it can handle it and keep running instead of crashing.
+# Provide Useful Feedback: You can show user-friendly messages instead of cryptic error messages.
+# Ensure Cleanup: Make sure important cleanup actions (like closing a file) are always performed.
+
+# Exception handling is done with the help of the following :-
+# 1. try block : It is used to handle the exception.
+# 2. catch block : Code that might throw an exception is placed inside a try block. If an exception occurs within this block, it is "thrown" and can be "caught" by a corresponding catch block.
