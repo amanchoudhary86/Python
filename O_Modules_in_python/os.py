22693b81d0109f281d0986291371a9257e5481da
amanchoudhary86 <amanchoudhary.1502@gmail.com>
2024-07-26 22:45:40 +0530

    26_July_2024

--- O_Modules_in_python/os.py
+++ O_Modules_in_python/os.py
@@ -4,4 +4,13 @@ import os
 
 print(dir(os) , "<-- This are all the directories present in this module")
 
-print(os.getcwd(), "<-- This is the current working directory")
+print(os.getcwd(), "<-- This is the current working directory") # cwd means current working directory
+# Output: C:\Users\Aman\OneDrive\Desktop\Python\O_Modules_in_python
+
+
+os.chdir("C:\Users\Aman\OneDrive\Desktop\Python")
+
+print(os.getcwd(), "<-- This is the current working directory") # cwd means current working directory
+# Output: C:\Users\Aman\OneDrive\Desktop\Python
+
+# As you can see above that the directory has been changed
