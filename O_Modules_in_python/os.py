8ce45d6e579a8ae62d120bd294dbba496fa20c7b
amanchoudhary86 <amanchoudhary.1502@gmail.com>
2024-07-29 20:47:51 +0530

    29_July_2024

--- O_Modules_in_python/os.py
+++ O_Modules_in_python/os.py
@@ -19,3 +19,4 @@ os.chdir(r"C:\Users\Aman\OneDrive\Desktop\Python\O_Modules_in_python")
 f = open("template.txt") # Note : Make sure you are in the correct directory to read this file otherwise it might raise an error.
 print("The file template.txt has been read")
 
+print(os.listdir(r"C:\Users\Aman\OneDrive\Desktop\Python\O_Modules_in_python"))
