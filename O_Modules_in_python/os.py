aac5cfe1b0631c006e30fc324c29ae25e040b872
amanchoudhary86 <amanchoudhary.1502@gmail.com>
2024-07-29 20:43:42 +0530

    29_July_2024

--- O_Modules_in_python/os.py
+++ O_Modules_in_python/os.py
@@ -16,5 +16,6 @@ print(os.getcwd(), "<-- This is the current working directory") # cwd means curr
 # As you can see above that the directory has been changed
 
 os.chdir(r"C:\Users\Aman\OneDrive\Desktop\Python\O_Modules_in_python")
-f = open("template.txt")
+f = open("template.txt") # Note : Make sure you are in the correct directory to read this file otherwise it might raise an error.
 print("The file template.txt has been read")
+
