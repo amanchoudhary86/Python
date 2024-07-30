fb58af7222e8f0ade35354e8690a20eb9e17a8ab
amanchoudhary86 <amanchoudhary.1502@gmail.com>
2024-07-30 20:16:49 +0530

    30_July_2024

---
+++ O_Modules_in_python/2_numpy_Module/numpy.py
@@ -0,0 +1,5 @@
+# Numpy :-
+
+# NumPy is a popular Python library used for working with arrays and numerical data. It provides tools for creating and manipulating large, multi-dimensional arrays and matrices, along with a collection of mathematical functions to operate on these arrays efficiently. NumPy is widely used in scientific computing, data analysis, and machine learning because it enables fast and flexible data processing.
+
+# Work in progress 🪧
