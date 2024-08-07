f11db0246a40398240824b9980a34f75894b43dc
amanchoudhary86 <amanchoudhary.1502@gmail.com>
2024-08-07 17:34:39 +0530

    7_August_2004

--- O_Modules_in_python/2_numpy_Module/B_Numpy.py
+++ O_Modules_in_python/2_numpy_Module/B_Numpy.py
@@ -1,7 +1,7 @@
 # Numpy :-
 
 # Firstly, What are arrays?
-# An array is a data structure that can hold a collection of elements, typically of the same data type, in a contiguous block of memory. Elements in an array are accessed using indices, with the first element typically located at index 0. Arrays are commonly used in programming to store and manipulate sequences of data efficiently.
+# An array is a data structure that can hold a collection of elements, typically of the same data type, in a contiguous block of memory. Elements in an array are accessed using indices, with the first element typically located at index 0. Arrays are commonly used to store and manipulate sequences of data efficiently.
 
 # NumPy is a popular Python library used for working with arrays and numerical data. It provides tools for creating and manipulating large, multi-dimensional arrays and matrices, along with a collection of mathematical functions to operate on these arrays efficiently. NumPy is widely used in scientific computing, data analysis, and machine learning because it enables fast and flexible data processing.
 
